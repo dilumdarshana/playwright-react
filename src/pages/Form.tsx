9824d5b1fc49231236fdfcf80096455b2e417a85
dilumdarshana <dilum.dar@gmail.com>
2025-02-01 23:25:16 +0530

    feat: initial set of files

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,13 +1,30 @@
-import { Link } from "react-router";
+import { useState, useRef } from 'react';
+import { Link } from 'react-router';
 
 export default function Form () {
+  const [comments, setComments] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (inputRef.current?.value) {
+      setComments([...comments, inputRef.current?.value]);
+      inputRef.current.value = '';
+    }
+  };
+
   return (
     <div className="max-w-2xl mx-auto">
+      <p>Form</p>
       <form>
-        <input className="border rounded-md" type="text" />
-        <input className="bg-blue-500 rounded-md px-4 text-white" type="button" value="Submit" />
+        <input ref={inputRef} className="border rounded-md" type="text" />
+        <input className="bg-blue-500 rounded-md px-4 text-white" onClick={handleClick} type="button" value="Submit" />
       </form>
       <Link className="link" to="/">Go to Home</Link>
+      <ul>
+        {comments.map(comment => (
+          <li key={comment}>{comment}</li>
+        ))}
+      </ul>
     </div>
   )
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Home page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173');
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test('Should have correct metadata and elements', async ({ page }) => {
+    await expect(page).toHaveTitle('React Playwright');
+    await expect(page.getByRole('heading', {
+      name: 'Welcome to the Home Page'
+    })).toBeVisible();
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('Should redirect to form page on click', async ({ page }) => {
+    await page.getByRole('link', { name: 'Go to Form'}).click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await expect(page).toHaveTitle('Form');
+  });
 });
